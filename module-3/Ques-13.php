da08508cf97f27957a987f87ffc1810e458bdf63
Mahaboob Alam <147504887+The-Coder-007@users.noreply.github.com>
2024-06-25 20:38:54 +0530

    Delete module-3 directory

--- module-3/Ques-13.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Sample array
-$array = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
-
-// Get a single random key
-$randomKey = array_rand($array);
-$randomValue = $array[$randomKey];
-
-echo "Random Value: $randomValue <br>";
