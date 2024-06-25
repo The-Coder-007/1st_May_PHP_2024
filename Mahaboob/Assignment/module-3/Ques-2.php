4db8cab91a0118dd3321765898c8acaebad43fc2
Mahaboob Alam <147504887+The-Coder-007@users.noreply.github.com>
2024-06-25 20:40:57 +0530

    Add files via upload

---
+++ Mahaboob/Assignment/module-3/Ques-2.php
@@ -0,0 +1,31 @@
+<?php
+
+//write a PHP program for find „Thursday‟ in week using switch Function.
+
+$day = "Thursday";
+
+switch($day){
+    case"Sunday":
+        echo "Its sunday";
+        break;
+    case"Monday":
+        echo "Its Monday";
+        break;
+    case"Tuesday":
+        echo "Its Tuesday";
+        break;
+    case"Wednesday":
+        echo "Its Wednesday";
+        break;
+    case"Thursday":
+        echo "Its Thursday";
+        break;
+    case"Friday":
+        echo "Its Friday";
+        break;
+    case"Saturday":
+        echo "Its Saturday";
+        break;
+    default:
+    echo "No Match !!";
+}
